8d41464768310173bc1b77f4ba501084033b8ab3
ExtemtedGold1 <wiktorbielecki31@gmail.com>
2021-05-07 12:47:22 +0200

    Miernio jazda

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-window.alert("Witaj w symulacji procesora.")
+const form = document.getElementById('form');
+form.addEventListener('submit', () => {
+    const ax = document.getElementById('AX');
+    const bx = document.getElementById('BX');
+    const cx = document.getElementById('CX');
+    const dx = document.getElementById('DX');
+    alert(ax.value +" " + bx.value + " " + cx.value + " " + dx.value)
+
+
+    var orders = document.getElementsByName('orders');
+    for(i=0; i<orders.length; i++){
+        if(orders[i].checked){
+            document.getElementById('wynik').innerHTML = orders[i].value;
+            break;
+        }
+    }
+
+})
