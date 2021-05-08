25e706a57bc8a4dd0c853460fa46516142288bc8
ExtemtedGold1 <wiktorbielecki31@gmail.com>
2021-05-08 19:17:17 +0200

    Help

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const form = document.getElementById('form');
+/*const form = document.getElementById('form');
 form.addEventListener('submit', () => {
     const ax = document.getElementById('AX');
     const bx = document.getElementById('BX');
@@ -14,5 +14,28 @@ form.addEventListener('submit', () => {
             break;
         }
     }
+})*/
+function formsumbit(){
+    const ax = document.getElementById('AX');
+    const bx = document.getElementById('BX');
+    const cx = document.getElementById('CX');
+    const dx = document.getElementById('DX');
+    
+    var orders = document.getElementsByName('orders');
+    var orders2 = document.getElementsByName('orders2');
+    var ordersValue;
+    var ordersValue2;
+    for(let i=0; i<orders.length; i++){
+        if(orders[i].checked){
+            document.getElementById('wynik').innerHTML = "wybrano " + orders[i].value;
+            ordersValue = orders[i].value;
+        }
+    }
+    for(let i=0; i<orders2.length; i++){
+        if(orders2[i].checked){
+            document.getElementById('wynik2').innerHTML = "wybrano " + orders2[i].value;
+            ordersValue2 = orders2[i].value;
+        }
 
-})
+    }    
+}
